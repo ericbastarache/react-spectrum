0fbd16168e5f160217e0b29ee799c4c6370e68e1
Eric Bastarache <eric_bastarache@hotmail.com>
2017-07-29 22:43:16 -0400

    Created preliminary colour picker component

---
+++ src/components/Spectrum/Spectrum.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Spectrum.css';
+
+class Spectrum extends Component {
+    render () {
+        return (
+            <div>Plug in Spectrum stuff here</div>
+        );
+    }
+}
+
+export default Spectrum;
