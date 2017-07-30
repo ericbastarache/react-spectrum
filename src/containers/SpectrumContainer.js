4625be7ff5e1e42a00a1799c915b26abcbd8fe3c
Eric Bastarache <eric_bastarache@hotmail.com>
2017-07-29 22:43:24 -0400

    Created preliminary colour picker container

---
+++ src/containers/SpectrumContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Spectrum from '../components/Spectrum/Spectrum';
+
+class SpectrumContainer extends Component {
+    render () {
+        return (
+            <Spectrum />
+        );
+    }
+}
+
+export default SpectrumContainer;
