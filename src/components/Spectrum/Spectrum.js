da9672f8e8a56b4bf4509996fd8e74b0e17afc4e
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-02 19:37:49 -0400

    Added handlers for Spectrum component

--- src/components/Spectrum/Spectrum.js
+++ src/components/Spectrum/Spectrum.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import './Spectrum.css';
 
 class Spectrum extends Component {
-    render () {
+    render() {
         return (
             <div>
                 <input
                     type="color"
                     onChange={this.props.handleChange}
                     onMouseMove={this.props.onSelect}
-                    />
+                    onMouseDown={this.props.handleMouseDown}
+                    onMouseUp={this.props.handleMouseUp}
+                />
             </div>
         );
     }
