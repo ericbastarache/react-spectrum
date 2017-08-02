7837e3eab568c10a3e8717381aa446878c3e6d4e
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-02 19:20:51 -0400

    Updated color picker component with preliminary handlers

--- src/components/Spectrum/Spectrum.js
+++ src/components/Spectrum/Spectrum.js
@@ -4,7 +4,13 @@ import './Spectrum.css';
 class Spectrum extends Component {
     render () {
         return (
-            <div>Plug in Spectrum stuff here</div>
+            <div>
+                <input
+                    type="color"
+                    onChange={this.props.handleChange}
+                    onMouseMove={this.props.onSelect}
+                    />
+            </div>
         );
     }
 }
