a76d611522cfa711859ba6c74f5079355da93278
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-02 19:38:21 -0400

    Updated SpectrumContainer with handlers to receive props from Specturm component

--- src/containers/SpectrumContainer.js
+++ src/containers/SpectrumContainer.js
@@ -3,11 +3,26 @@ import React, { Component } from 'react';
 import Spectrum from '../components/Spectrum/Spectrum';
 
 class SpectrumContainer extends Component {
+
+    handleChange = (e) => {
+
+    }
+
+    onSelect = (e) => {
+
+    }
+
+    handleMouseDown = (e) => {
+
+    }
+
     render() {
         return (
             <Spectrum
                 handleChange={this.handleChange}
+                handleMouseDown={this.handleMouseDown}
                 onSelect={this.onSelect}
+                handleMouseUp={this.handleMouseUp}
 
             />
         );
