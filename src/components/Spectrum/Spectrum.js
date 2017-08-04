1da00b6dcfbed0f4fb750da2f1eeb47d2ab12912
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-04 16:13:51 -0400

    Updated functions and prop functions

--- src/components/Spectrum/Spectrum.js
+++ src/components/Spectrum/Spectrum.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import './Spectrum.css';
+import React, { Component } from "react";
+import "./Spectrum.css";
 
 class Spectrum extends Component {
-    render() {
-        return (
-            <div>
-                <input
-                    type="color"
-                    onChange={this.props.handleChange}
-                    onMouseMove={this.props.onSelect}
-                    onMouseDown={this.props.handleMouseDown}
-                    onMouseUp={this.props.handleMouseUp}
-                />
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<input
+					type="color"
+					onInput={this.props.handleInput}
+					onMouseMove={this.props.onSelect}
+					onMouseDown={this.props.handleMouseDown}
+					onMouseUp={this.props.handleMouseUp}
+				/>
+			</div>
+		);
+	}
 }
 
 export default Spectrum;
